cb4f1a487350bfd2212696db9b8610c4f8489d6b
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2023-07-22 21:05:14 +0530

    made changes to css

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const bulb=document.getElementById("light");
-const input=document.getElementById("clickMe").addEventListener("click",function(){
-    if(bulb.src.match("images/off.gif")){
-        bulb.src="images/on.gif";
+const bulb = document.getElementById("light");
+const input = document
+  .getElementById("clickMe")
+  .addEventListener("click", function () {
+    if (bulb.src.match("images/off.gif")) {
+      bulb.src = "images/on.gif";
     } else {
-        bulb.src="images/off.gif";
+      bulb.src = "images/off.gif";
     }
-});
+  });
